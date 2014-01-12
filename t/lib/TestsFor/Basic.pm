4925f66cdece737c9daf434c75d3500a47dcbc8d
Ovid <curtis_ovid_poe@yahoo.com>
2014-01-12 12:24:08 +0100

    The current_method is now available on our test report

--- t/lib/TestsFor/Basic.pm
+++ t/lib/TestsFor/Basic.pm
@@ -1,17 +1,38 @@
 package TestsFor::Basic;
 use Test::Class::Moose;
 
+has [ 'setup_class_found', 'setup_method_found' ] => (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+sub test_setup {
+    my $test          = shift;
+    my $report        = $test->test_report;
+    my $current_class = $report->current_class;
+    $test->setup_class_found( $current_class->name );
+    $test->setup_method_found( $report->current_method->name );
+}
+
 sub test_me {
     my $test  = shift;
     my $class = ref $test;
     ok 1, "test_me() ran ($class)";
     ok 2, "this is another test ($class)";
+    is $test->setup_class_found, $test->test_class,
+      'test_setup() should know our current class name';
+    is $test->setup_method_found, 'test_me',
+      '... and our current method name';
 }
 
 sub test_this_baby {
     my $test  = shift;
     my $class = ref $test;
     is 2, 2, "whee! ($class)";
+    is $test->setup_class_found, $test->test_class,
+      'test_setup() should know our current class name';
+    is $test->setup_method_found, 'test_this_baby',
+      '... and our current method name';
 }
 
 sub test_reporting {
@@ -22,6 +43,10 @@ sub test_reporting {
       'current_class() should report the correct class name';
     is $report->current_method->name, 'test_reporting',
       '... and we should also be able to get the current method name';
+    is $test->setup_class_found, $test->test_class,
+      'test_setup() should know our current class name';
+    is $test->setup_method_found, 'test_reporting',
+      '... and our current method name';
 }
 
 1;
