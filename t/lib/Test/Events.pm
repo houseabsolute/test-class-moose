c5c68b45dd83d05b135c46156f6fd4551e5c7dc3
Dave Rolsky <autarch@urth.org>
2016-02-24 09:20:41 -0500

    Add a sub to dump events in a compact form to Test::Events

--- t/lib/Test/Events.pm
+++ t/lib/Test/Events.pm
@@ -31,3 +31,59 @@ sub _diag_exception_events {
         }
     }
 }
+
+our $Indent = 0;
+
+sub dump_events {
+    my $events = shift;
+
+    for my $event ( @{$events} ) {
+        if ( $event->isa('Test2::Event::Subtest') ) {
+            _d(     'Subtest(name: '
+                  . $event->name . ', pass:'
+                  . $event->pass
+                  . ') {' );
+            {
+                local $Indent = $Indent + 1;
+                dump_events( $event->subevents );
+            }
+            _d('}');
+        }
+        elsif ( $event->isa('Test2::Event::Diag') ) {
+            _d( 'Diag(' . _e($event->message) . ')' );
+        }
+        elsif ( $event->isa('Test2::Event::Note') ) {
+            _d( 'Note(' . _e($event->message) . ')' );
+        }
+        elsif ( $event->isa('Test2::Event::Exception') ) {
+            _d( 'Exception(' . $event->error . ')' );
+        }
+        elsif ( $event->isa('Test2::Event::Ok') ) {
+            _d(     'Ok(pass:'
+                  . $event->pass
+                  . ( defined $event->name ? ', name:' . $event->name : q{} )
+                  . ')' );
+        }
+        elsif ( $event->isa('Test2::Event::Plan') ) {
+            _d( 'Plan(max:' . $event->max . ')' );
+        }
+        else {
+            my ($type) = ( ref $event ) =~ /Test2::Event::(.+)/;
+            _d($type);
+        }
+    }
+}
+
+sub _e {
+    my $str = shift;
+    $str =~ s/\n/\\n/g;
+    $str =~ s/\r/\\r/g;
+    return $str;
+}
+
+sub _d {
+    my $i = q{ } x $Indent;
+    diag( $i . $_[0] );
+}
+
+1;
